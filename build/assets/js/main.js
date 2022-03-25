3ef6c2aa0ff3e65b380107765d30fa664e78edc8
Tâm Võ <97896181+tamvo0610@users.noreply.github.com>
2022-03-25 21:28:52 +0700

    Update main.js

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -16,3 +16,22 @@ document.onscroll = function(){
         scrollNavBar.style.boxShadow = null
     }
 }
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
