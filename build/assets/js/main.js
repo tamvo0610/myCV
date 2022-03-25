ca8d63b9e76698fe7b8461def088d61d868a2723
tamvo0610 <tamvo0610@gmail.com>
2022-03-25 16:36:48 +0700

    đá

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,10 +1,10 @@
 /* ========== NAVBAR ON SCROLL ========== */
 document.onscroll = function(){
-    const scrollNavBar = document.querySelector("#navbar")
+    const scrollNavBar = document.querySelector("#header")
     const scrollWidth = window.scrollY
     if(scrollWidth > 0){
-        scrollNavBar.style.backgroundColor = "#fff"
-        scrollNavBar.style.color = "#000000"
+        scrollNavBar.style.backgroundColor = "hsl(219, 48%, 8%)"
+        scrollNavBar.style.color = "hsl(219, 8%, 75%)"
         scrollNavBar.style.lineHeight = "2rem"
         scrollNavBar.style.animation = "slideDown ease 0.3s"
         scrollNavBar.style.boxShadow = "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
