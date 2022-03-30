89ea713bcac452afb1d3b434c63de72115e79672
tamvo0610 <tamvo0610@gmail.com>
2022-03-30 16:00:47 +0700

    125615

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,28 +1,28 @@
-/* ========== NAVBAR ON SCROLL ========== */
+const $ = document.querySelector.bind(document),
+    $$ = document.querySelectorAll.bind(document)
+/* ========== Header ON SCROLL ========== */
 document.onscroll = function(){
-    const scrollNavBar = document.querySelector("#header")
+    const scrollHeader = document.querySelector("#header")
     const scrollWidth = window.scrollY
     if(scrollWidth > 0){
-        scrollNavBar.style.backgroundColor = "hsla(219, 32%, 16%, .9)"
-        scrollNavBar.style.color = "hsl(219, 8%, 75%)"
-        scrollNavBar.style.lineHeight = "2rem"
-        scrollNavBar.style.animation = "slideDown ease 0.3s"
-        scrollNavBar.style.boxShadow = "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
+        scrollHeader.style.backgroundColor = "hsla(219, 32%, 16%, .9)"
+        scrollHeader.style.color = "hsl(219, 8%, 75%)"
+        scrollHeader.style.lineHeight = "2rem"
+        scrollHeader.style.animation = "slideDown ease 0.3s"
+        scrollHeader.style.boxShadow = "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
     } else {
-        scrollNavBar.style = null
+        scrollHeader.style = null
     }
 }
 
+/* ========== Navbar on Mobile ========== */
 const sections = document.querySelectorAll('section[id]')
-
 function scrollActive(){
     const scrollY = window.pageYOffset
-
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
               sectionTop = current.offsetTop - 58,
               sectionId = current.getAttribute('id')
-
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
@@ -31,3 +31,26 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+/* ========== Typed Text ========== */
+var typed = new Typed('.text-slider-items', {
+	strings: ["Frontend Developer ."],
+	typeSpeed: 80,
+	loop: true,
+	backDelay: 1100,
+	backSpeed: 30
+});  
+/* ========== SHOW/HIDE Project Detail ========== */
+const elementProject = $$("#project-section .container .main .content .card-item")
+elementProject.forEach(function(value,index){
+    const show = value.querySelector(".js-project-show")
+    const hide = value.querySelector(".js-project-hide")
+    const element = value.querySelector(".project-item")
+    console.log(hide)
+    show.onclick = function (){
+        element.classList.add("active")
+        element.style.animation = "slide-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
+    }
+    hide.onclick = function (){
+        element.style.animation = "scale-out-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
+    }
+})
